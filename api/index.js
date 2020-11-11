dedc65a8fa857074a8a966c7fb7bbce86239df6e
nakyungwon <kwna@mycelebs.com>
2020-11-11 11:36:39 +0900

    init ..

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+// export default (req, res) => {
+//   console.log('testest')
+//   res.send('훌륭합니다~~ ' + Math.random())
+// }
+app.get('/', (req, res, next) => {
+  res.send('훌륭해요! ' + Math.random())
+})
+
+module.exports = {
+  path: '/api',
+  handler: app
+}
---
+++ pages/test.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card>
+    <v-card-title>
+      hello???
+    </v-card-title>
+    <v-card-text>
+      <v-textarea v-model="txt"/>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="test">
+        test
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+
+<script>
+export default {
+  name: "test"
+  ,data() {
+    return {
+      txt: ''
+    }
+  }
+  ,methods: {
+    async test() {
+      const r = await this.$axios.get('/api')
+      // console.log('adfasdf')
+      this.txt = r.data
+    }
+  }
+}
+</script>
+
+<!--<style scoped>-->
+<!--</style>-->
